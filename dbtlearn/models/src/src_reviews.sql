6d57288ae0ce1f117ac6f7b1f1412c68077f22f4
yimengke <yimengkeee@gmail.com>
2025-07-11 00:36:30 +0200

    Created staging layer

---
+++ dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,11 @@
+WITH raw_reviews AS (
+    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+)
+
+SELECT
+    listing_id,
+    date AS review_date,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM
+    raw_reviews
