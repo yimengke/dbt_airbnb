24563b6c107fd004e4b2f5dc819b7bdbc46937ac
yimengke <yimengkeee@gmail.com>
2025-07-11 01:00:02 +0200

    Created Dim layer

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+WITH src_hosts AS (
+    SELECT * from {{ ref("src_hosts")}}
+)
+
+SELECT
+    host_id,
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM src_hosts
