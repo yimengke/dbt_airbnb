1ec385c3f8d27abfb833ec30d75f65238724893f
yimengke <yimengkeee@gmail.com>
2025-07-24 23:22:08 +0200

    Added macros

--- dbtlearn/tests/consistent_created_at.sql
+++ dbtlearn/tests/consistent_created_at.sql
@@ -2,4 +2,4 @@ SELECT
 *
 FROM {{ref('fct_reviews')}}  r
 LEFT JOIN {{ ref('dim_listings_cleansed')}} l ON r.listing_id = l.listing_id
-WHERE r.review_date < l.created_at
+WHERE r.review_date <= l.created_at
